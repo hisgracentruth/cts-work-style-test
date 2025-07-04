453fd7015517e09a09e1692f69a188a50d97d23d
hisgracentruth <106937138+hisgracentruth@users.noreply.github.com>
2025-07-04 12:26:39 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WorkStyleTest from './WorkStyleTest';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WorkStyleTest />
+    </div>
+  );
+}
+
+export default App;
